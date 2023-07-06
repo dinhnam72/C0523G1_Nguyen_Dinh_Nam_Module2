3693a8a64aa3221c75cbff96c12580b199a86655
dinhnam72 <dinhnam20162011@gmail.com>
2023-07-06 11:38:14 +0700

    nộp bài tập giải thuật

--- C0523G1_Nguyen_Dinh_Nam_Module2/src/giaithuat/Bt1.java
+++ C0523G1_Nguyen_Dinh_Nam_Module2/src/giaithuat/Bt1.java
@@ -1,2 +1,16 @@
-package giaithuat;public class Bt1 {
+package giaithuat;
+
+public class Bt1 {
+    public static void main(String[] args) {
+        for (int i = -4; i <= 4; i++) {
+            for (int j = 1; j < 11; j++) {
+                if (Math.abs(i) + j == 10 || j - Math.abs(i) == 1) {
+                    System.out.print(5 - Math.abs(i));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
